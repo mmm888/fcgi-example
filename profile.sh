d5ece137af3cc4d693bbc21ddc80f354b00da521
mmm888 <silversoul.wt@gmail.com>
2018-02-28 17:29:45 +0900

    fix

--- profile.sh
+++ profile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh                                                                                                                                                                                                                                     
-                                                                                                                                                                                                                                              
+
+CONTAINER_IMAGE=XXX
 PROFILE_PATH=/root/XXX                                                                                                                                                                                                      
 BIN_PATH=XXX                                                                                                                                                                                                                        
                                                                                                                                                                                                                                               
-docker cp "lenet-web:${PROFILE_PATH}" .                                                                                                                                                                                                       
+docker cp "${CONTAINER_IMAGE}:${PROFILE_PATH}" .                                                                                                                                                                                                       
 go tool pprof "${BIN_PATH}" $(basename "${PROFILE_PATH}") 
